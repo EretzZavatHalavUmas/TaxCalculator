60f7595ee433af96847c0739aecda2bf429889ba
EretzZavatHalavUmas <96304175+EretzZavatHalavUmas@users.noreply.github.com>
2021-12-18 12:27:51 +0200

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -138,4 +138,6 @@ def success():
 
 if __name__ == '__main__':
     app.debug = True
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+   # app.run()
