65f41319b7f8b547fcd6bc221ac58bf39332ac9d
EretzZavatHalavUmas <96304175+EretzZavatHalavUmas@users.noreply.github.com>
2021-12-18 11:54:45 +0200

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -49,7 +49,7 @@ class Data(db.Model):
         self.totalTax_ = totalTax_
         self.totalTaxPer_ = totalTaxPer_
 
-@app.route("/app")
+@app.route("/")
 def index():
     return render_template("index.html")
 
