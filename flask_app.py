b560afe667744077f5229265e5f3f7b87b30ea64
EretzZavatHalavUmas <96304175+EretzZavatHalavUmas@users.noreply.github.com>
2021-12-23 22:18:05 +0200

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -11,7 +11,7 @@ import os
 from send_email import send_email
 
 app=Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"]='mysql://Ron:MREngineering1$@http://data2.chto6pl4dabw.us-east-2.rds.amazonaws.com:3306/data2'
+app.config["SQLALCHEMY_DATABASE_URI"]='mysql://Ron:MREngineering1$@http://data2.chto6pl4dabw.us-east-2.rds.amazonaws.com:3306/data2inputs'
 db=SQLAlchemy(app)
 
 
