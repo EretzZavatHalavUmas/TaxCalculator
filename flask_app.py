e250ec4b99ba7e79689aad550215ec68647d0c59
EretzZavatHalavUmas <96304175+EretzZavatHalavUmas@users.noreply.github.com>
2021-12-25 16:46:35 +0200

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -149,6 +149,10 @@ def about():
 @app.route("/home")
 def home():
     return render_template("home.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
     
 if __name__ == '__main__':
    app.debug = True
