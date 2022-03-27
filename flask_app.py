80afba16c72dd68ccc4bdf74c8342f0e609299b0
Matan <matan4900@gmail.com>
2022-03-27 15:07:32 +0300

    py changes - working

--- flask_app.py
+++ flask_app.py
@@ -6,14 +6,16 @@ from taxCalculator import *
 import os
 from send_email import send_email
 import pymysql
-#from sqlalchemy.sql import func
-#from requests import get
-#import urllib.request
+
+# from sqlalchemy.sql import func
+# from requests import get
+# import urllib.request
 
 
-app =  Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"]='mysql://Ron:MREngineering1$@data2.chto6pl4dabw.us-east-2.rds.amazonaws.com:3306/data2inputs'
-db=SQLAlchemy(app)
+app = Flask(__name__)
+app.config[
+    "SQLALCHEMY_DATABASE_URI"] = 'mysql://Ron:MREngineering1$@data2.chto6pl4dabw.us-east-2.rds.amazonaws.com:3306/data2inputs'
+db = SQLAlchemy(app)
 
 
 class Data(db.Model):
@@ -33,7 +35,8 @@ class Data(db.Model):
     totalTax_ = db.Column(db.Float())
     totalTaxPer_ = db.Column(db.Float())
 
-    def __init__(self, date_, time_, salary_, nz_, rent_, arnona_, expanses_, gasoline_, car_, totalDirectTax_, totalDirectTaxPer_,  totalTax_, totalTaxPer_):
+    def __init__(self, date_, time_, salary_, nz_, rent_, arnona_, expanses_, gasoline_, car_, totalDirectTax_,
+                 totalDirectTaxPer_, totalTax_, totalTaxPer_):
         self.date_ = date_
         self.time_ = time_
         self.salary_ = salary_
@@ -48,16 +51,18 @@ class Data(db.Model):
         self.totalTax_ = totalTax_
         self.totalTaxPer_ = totalTaxPer_
 
+
 port = int(os.environ.get("PORT", 5000))
-        
+
+
 @app.route("/calc")
 def calc():
     return render_template("calc.html")
 
+
 @app.route("/success", methods=['POST'])
 def success():
     if request.method == 'POST':
-        
         salary = float(request.form["salary"])
         nz = float(request.form["nz"])
         rent = float(request.form["rent"])
@@ -66,19 +71,19 @@ def success():
         gasoline = float(request.form["gasoline"])
         car = float(request.form["car"])
 
-        mh = MH(salary,nz)
-        pension = salary*0.185
+        mh = MH(salary, nz)
+        pension = salary * 0.185
         bl, bb = BL(salary)
-        vat = VAT(expanses,gasoline)
+        vat = VAT(expanses, gasoline)
         gasTax = gasolineTax(gasoline)
         carTax = carTaxCalc(car)
-        totalExp = (expanses-vat)+(gasoline-gasTax) #calculate car monthly expense
-        totalExpanseTax = vat+gasTax+carTax+arnona
-        totalDirectTax = mh+bl+bb
-        netSalary = salary - totalDirectTax - 0.06*salary
+        totalExp = (expanses - vat) + (gasoline - gasTax)  # calculate car monthly expense
+        totalExpanseTax = vat + gasTax + carTax + arnona
+        totalDirectTax = mh + bl + bb
+        netSalary = salary - totalDirectTax - 0.06 * salary
         totalTax = totalDirectTax + totalExpanseTax
-        totalDirectTaxPer = totalDirectTax/salary*100
-        totalTaxPer = totalTax/salary*100
+        totalDirectTaxPer = totalDirectTax / salary * 100
+        totalTaxPer = totalTax / salary * 100
 
         now = datetime.now()
         from_zone = tz.gettz('UTC')
@@ -87,13 +92,13 @@ def success():
         now = now.astimezone(to_zone)
         date = now.strftime("%d/%m/%Y")
         time = now.strftime("%H:%M:%S")
-        data = Data(date, time, salary, nz, rent, arnona, expanses, gasoline, car, totalDirectTax, totalDirectTaxPer, totalTax, totalTaxPer)
+        data = Data(date, time, salary, nz, rent, arnona, expanses, gasoline, car, totalDirectTax, totalDirectTaxPer,
+                    totalTax, totalTaxPer)
         db.session.add(data)
         db.session.commit()
-        #average_height = db.session.query(func.avg(Data.height_)).scalar()
-        #average_height = round(average_height,0)
-        #count = db.session.query(Data.height_).count()
-        
+        # average_height = db.session.query(func.avg(Data.height_)).scalar()
+        # average_height = round(average_height,0)
+        # count = db.session.query(Data.height_).count()
 
         return render_template("success.html",
                                mhText="mas haknasa is %s ILS " % round(mh),
@@ -102,53 +107,57 @@ def success():
                                vatText="expanses Tax is %s ILS " % round(vat),
                                gasolineText="gas Tax is %s ILS " % round(gasTax),
                                carText="car Tax is %s ILS" % round(carTax),
-                               totalDirectTaxText = "המיסים הישירים - ₪ %s "
-                               % round(totalDirectTax),
-                               totalDirectTaxPerText = "שמהווים כ-%% %s מההכנסה"
-                               % round(totalDirectTaxPer),
-                               totalTaxText = "סך כל המיסים - ₪ %s "
-                               % round(totalTax),
-                               totalTaxPerText = "שמהווים כ-%% %s מההכנסה"
-                               % round(totalTaxPer),
-                               DATA = [['Category', 'ILS'],
-                                      ['מס הכנסה',
+                               totalDirectTaxText="המיסים הישירים - ₪ %s "
+                                                  % round(totalDirectTax),
+                               totalDirectTaxPerText="שמהווים כ-%% %s מההכנסה"
+                                                     % round(totalDirectTaxPer),
+                               totalTaxText="סך כל המיסים - ₪ %s "
+                                            % round(totalTax),
+                               totalTaxPerText="שמהווים כ-%% %s מההכנסה"
+                                               % round(totalTaxPer),
+                               DATA=[['Category', 'ILS'],
+                                     ['מס הכנסה',
                                       round(mh)],
-                                      ['ביטוח לאומי',
+                                     ['ביטוח לאומי',
                                       round(bl)],
-                                      ['ביטוח בריאות',
+                                     ['ביטוח בריאות',
                                       round(bb)]
-                                    ],
-                               DATA2 = [['Category', 'ILS'],
-                                      ['מס הכנסה',     round(mh)],
-                                      ['ביטוח לאומי',      round(bl)],
-                                      ['ביטוח בריאות',  round(bb)],
+                                     ],
+                               DATA2=[['Category', 'ILS'],
+                                      ['מס הכנסה', round(mh)],
+                                      ['ביטוח לאומי', round(bl)],
+                                      ['ביטוח בריאות', round(bb)],
                                       ['מע"מ', round(vat)],
-                                      ['מס בלו',    round(gasTax)],
-                                      ['מס על רכב',    round(carTax)],
-                                      ['ארנונה',    round(arnona)]
-                                    ],
-                               DATA3 = [['Category', 'ILS'],
-                                      ['מס הכנסה',     round(mh)],
-                                      ['ביטוח לאומי',      round(bl)],
-                                      ['ביטוח בריאות',  round(bb)],
+                                      ['מס בלו', round(gasTax)],
+                                      ['מס על רכב', round(carTax)],
+                                      ['ארנונה', round(arnona)]
+                                      ],
+                               DATA3=[['Category', 'ILS'],
+                                      ['מס הכנסה', round(mh)],
+                                      ['ביטוח לאומי', round(bl)],
+                                      ['ביטוח בריאות', round(bb)],
                                       ['מע"מ', round(vat)],
-                                      ['מס בלו',    round(gasTax)],
-                                      ['מס על רכב',    round(carTax)]
-                                    ])
+                                      ['מס בלו', round(gasTax)],
+                                      ['מס על רכב', round(carTax)]
+                                      ])
+
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
-    
+
+
 if __name__ == '__main__':
-   app.debug = True
-   app.run(host='0.0.0.0', port=port)
-   #app.run()
+    app.debug = True
+    app.run(host='0.0.0.0', port=port)
+    # app.run()
